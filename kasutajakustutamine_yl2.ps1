4d57b16b415c6b7e4ca6c3fd489060c2d28565b8
Daniel <daniel-erik.kiuru@voco.ee>
2024-12-02 13:54:03 +0200

    Kasutaja kustutamine command lineis

---
+++ kasutajakustutamine_yl2.ps1
@@ -0,0 +1,14 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+$kasutajanimi = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+
+try{
+    Remove-LocalUser $kasutajanimi -ea Stop | Out-Null
+    Write-Host "Kustutatav kasutaja on $kasutajanimi"
+    Write-Host "Kasutaja $kasutajanimi on kustutatud"
+}
+catch{
+    Write-Host "Kasutaja kustutamisega tekkis probleem" -ForegroundColor Red 
+    Write-Host "Probleemi detailid: $_" -ForegroundColor Red
+}
