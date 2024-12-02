eabe477ac4c90755d5684e2080baeea205c43bcf
Daniel <daniel-erik.kiuru@voco.ee>
2024-12-02 12:40:14 +0200

    Kasutaja loomine 2

---
+++ kasutajaloomine_2.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    $username
+    $fullname
+    $description
+)
+
+$userpassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
+
+New-LocalUser $username -Password $userpassword -FullName $fullname -Description $description
