c061eb342cab8f1323930e6be27212ec4eff35c6
Daniel <daniel-erik.kiuru@voco.ee>
2024-12-02 13:50:25 +0200

    Kasutaja loomine läbi command linei

---
+++ kasutajaloomine_yl1.ps1
@@ -0,0 +1,15 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+$kasutajanimi = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+$Kontokirjeldus = "Lokaalne konto"
+$parool = "Parool1!" | ConvertTo-SecureString -AsPlainText -Force
+
+try{
+    New-LocalUser $kasutajanimi -Password $parool -Description $Kontokirjeldus -ea Stop | Out-Null
+    Write-Host "Loodav kasutaja on $kasutajanimi"
+}
+catch{
+    Write-Host "Kasutaja loomisega tekkis probleem" -ForegroundColor Red 
+    Write-Host "Probleemi detailid: $_" -ForegroundColor Red
+}
