f11fbb433a14da2e785f37e52eb6192e8d3f8264
Daniel <daniel-erik.kiuru@voco.ee>
2024-12-02 13:17:34 +0200

    Programm mis loeb failist kasutaja andmed

---
+++ kasutajaloomine_failist.ps1
@@ -0,0 +1,16 @@
+﻿$fail = "C:\Users\Daniel\ps_haldus\kasutajad.csv"
+
+$kasutajad = Import-Csv $fail -Encoding Default -Delimiter ";"
+
+foreach ($kasutaja in $kasutajad)
+{
+$kasutajanimi = $kasutaja.KasutajaNimi
+$Taisinimi = $Kasutaja.TaisNimi
+$Kontokirjeldus = $kasutaja.KontoKirjeldus
+$parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+New-LocalUser -Name $kasutajanimi `
+    -Password $parool `
+    -FullName "$Taisinimi" `
+    -Description "$Kontokirjeldus" `
+    -ErrorAction SilentlyContinue
+}
